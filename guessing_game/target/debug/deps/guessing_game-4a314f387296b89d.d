cd6f46ef909c793ac9381ab32b10b54007987db6
github username <harr.hughes@gmail.com>
2021-10-11 23:19:43 +0100

    guessing game started

---
+++ guessing_game/target/debug/deps/guessing_game-4a314f387296b89d.d
@@ -0,0 +1,5 @@
+/Users/Harrison/Development/code/rust/guessing_game/target/debug/deps/guessing_game-4a314f387296b89d.rmeta: src/main.rs
+
+/Users/Harrison/Development/code/rust/guessing_game/target/debug/deps/guessing_game-4a314f387296b89d.d: src/main.rs
+
+src/main.rs:
