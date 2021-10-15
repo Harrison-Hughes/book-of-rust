1e59301b5684a7905ed7a7bfed6c6584b3bf8ce7
Harrison <harrison.hughes@cookieshq.co.uk>
2021-10-15 16:03:58 +0100

    rectangles complete

---
+++ rectangles/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+/Users/harrisonhughes/code/rust/rectangles/target/debug/deps/structs: src/main.rs
+
+/Users/harrisonhughes/code/rust/rectangles/target/debug/deps/structs.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/harrisonhughes/code/rust/rectangles/target/debug/structs: /Users/harrisonhughes/code/rust/rectangles/src/main.rs
