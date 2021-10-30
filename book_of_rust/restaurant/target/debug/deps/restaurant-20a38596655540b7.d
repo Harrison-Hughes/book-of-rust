e687d5678832e4ddfb1d97e1b65bcbd417ba262a
github username <harr.hughes@gmail.com>
2021-10-30 16:59:32 +0100

    book of rust directory created

---
+++ book_of_rust/restaurant/target/debug/deps/restaurant-20a38596655540b7.d
@@ -0,0 +1,7 @@
+/Users/Harrison/Development/code/rust/restaurant/target/debug/deps/restaurant-20a38596655540b7.rmeta: src/lib.rs
+
+/Users/Harrison/Development/code/rust/restaurant/target/debug/deps/librestaurant-20a38596655540b7.rlib: src/lib.rs
+
+/Users/Harrison/Development/code/rust/restaurant/target/debug/deps/restaurant-20a38596655540b7.d: src/lib.rs
+
+src/lib.rs:
